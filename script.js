3ffd2a246a2363b2f51c38c84755f468d4e9dc7b
Shubham Kr. Mishra <shubham.mishrakiit@gmail.com>
2023-10-10 15:49:06 +0530

    implemenation of modal close

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 'use strict';
 const modal = document.querySelector('.modal');
-const overlay = document.querySelector('overlay');
+const overlay = document.querySelector('.overlay');
 const closeModal = document.querySelector('.close-modal');
 
 const showModal = document.querySelectorAll('.show-modal');
 
+
 for(let i=0;i<showModal.length;i++)
 {
-    console.log(showModal[i].textContent);
+    showModal[i].addEventListener('click',function(){
+        console.log('Button Clicked');
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    })
+
 }
+const btncloseModal=function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+closeModal.addEventListener('click',btncloseModal);
+overlay.addEventListener('click',btncloseModal);
+
+document.addEventListener('keydown',function(e){
+    console.log(e.key);
+    if(e.key === 'Escape' && !modal.classList.contains('hidden'))
+    {
+         btncloseModal();
+    }
+})
